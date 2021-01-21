2dbf565d75a6c40645321361a3830fdaf729bafd
dlucascabral <dlucascabral@gmail.com>
2021-01-21 14:16:54 -0300

    Inicio do CRUD

---
+++ Pages/Booklist/Create.cshtml.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Model;
+
+
+namespace BookListRazor.Pages.Booklist
+{
+    public class CreateModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CreateModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public Book Book { get; set; }
+
+        public void OnGet()
+        {
+            
+        }
+    }
+}
